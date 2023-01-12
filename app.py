366de22d82afd2d12fbdeed2717b98587d83ba54
Eleazar Udo <elzoremmanuel@gmail.com>
2023-01-13 00:43:11 +0200

    the images saves in the folder

--- app.py
+++ app.py
@@ -12,19 +12,28 @@ SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
     form = UploadForm()
-    return render_template('home.html', form=form);
+    if request.method == "GET":
+        return render_template('home.html', form=form);
+    elif request.method == "POST":
+        folder = 'images/'
+        number = request.form['color_count']
+        if request.files['file']:
+            f = request.files['file']
+            filename = secure_filename(f.filename)
+            print (number)
+            new_path = os.path.join(folder, filename)
+            print(new_path)
+            f.save(new_path)
+            return render_template('home.html', form=form)    
+    
+
 
 @app.route("/features")
 def features():
     return render_template('features.html')
-
-#@app.route('/upload', methods=['POST'])
-#def upload_file():
- #   if request.method == 'POST':
-        #f = request.files[]
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000);
--- forms.py
+++ forms.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import IntegerField
+from wtforms import IntegerField, SubmitField
 from wtforms.validators import DataRequired
 
 class UploadForm(FlaskForm):
-    file = FileField('image', validators=[FileRequired(),
+    file = FileField('Upload Image: ', validators=[FileRequired(),
                                           FileAllowed(['jpg', 'png'], 'Images only!')
                                           ])
-    color_count = IntegerField('number', validators=[DataRequired()])
+    color_count = IntegerField('Number of colors: ', validators=[DataRequired()])
+    submit = SubmitField()
     
     
     
